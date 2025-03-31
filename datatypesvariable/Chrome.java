b3b3ae2e942dfdb5ff4f5030f0bf78d282ac23a0
Pavitz <100362155+Pavitz@users.noreply.github.com>
2025-03-31 23:24:06 +0530

    Add files via upload
    Datatype assignment

---
+++ datatypesvariable/Chrome.java
@@ -0,0 +1,25 @@
+package datatypesvariable;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//float	
+		float version=91.0f;
+		String developer="Google";
+		boolean isBeta= false;
+		int releaseyear=2008;
+        char shortcutKey = 'C';
+		
+		
+			System.out.println("chrome version:"+version);
+			System.out.println("developer:"+developer);	
+			System.out.println("Is Beta:"+isBeta);
+			System.out.println("releaseyear:"+releaseyear);
+	        System.out.println("ShortcutKey: " + shortcutKey);
+
+	}
+
+}
+	
