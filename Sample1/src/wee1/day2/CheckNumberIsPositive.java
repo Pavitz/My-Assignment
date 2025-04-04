80bf8639da2b5ba4f8ed9ae4cdc1688fca2c3f84
Pavitz <100362155+Pavitz@users.noreply.github.com>
2025-04-04 12:36:42 +0530

    Add files via upload
    Checknumberispositive

---
+++ Sample1/src/wee1/day2/CheckNumberIsPositive.java
@@ -0,0 +1,19 @@
+package wee1.day2;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=10;
+		if (n>0) {
+		System.out.println("Number is Positive");
+		}
+		else if (n<0) {
+			System.out.println("Number is Negative");
+		}
+		else {
+			System.out.println("number is zero");
+		}
+	}
+
+}
