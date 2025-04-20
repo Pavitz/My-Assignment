104c46406551bf776e12c2aa61e21543bcb73682
Pavitz <100362155+Pavitz@users.noreply.github.com>
2025-04-20 20:35:13 +0530

    Add files via upload
    commit changes

---
+++ methods/src/week2/CheckNumberIsPositive.java
@@ -0,0 +1,26 @@
+package week2;
+
+public class CheckNumberIsPositive {
+		  
+		    int version = 10;
+		    public void checkNumber(int number) {
+		     
+		        if (number > 0) {
+		            System.out.println(number + " is a positive number.");
+		        } else 
+		      
+		            System.out.println(number + " is not a positive number.");
+		        }
+		
+		    public static void main(String[] args) {
+		        CheckNumberIsPositive checker = new CheckNumberIsPositive();
+		        System.out.println("Version: " + checker.version);
+
+		        checker.checkNumber(5);   
+		        checker.checkNumber(-3);  
+		        checker.checkNumber(0);  
+		    }	
+
+	}
+
+
